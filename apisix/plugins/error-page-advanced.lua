bbbd62bbe58905ccaa99ed7585bc6e63b6fab053
mikyll <righi.michy@gmail.com>
2025-04-17 12:17:33 +0200

    fix: set status_text via core.ctx.register_var()

--- apisix/plugins/error-page-advanced.lua
+++ apisix/plugins/error-page-advanced.lua
@@ -32,15 +32,14 @@ local plugin_author      = {
   url = "https://github.com/mikyll",
 }
 
-
-local ngx             = ngx
-local core            = require("apisix.core")
-local apisix_plugin   = require("apisix.plugin")
-local apisix_utils    = require("apisix.core.utils")
+local ngx                = ngx
+local core               = require("apisix.core")
+local apisix_plugin      = require("apisix.plugin")
+local apisix_utils       = require("apisix.core.utils")
 
 -- TODO: add error_range for other pages
 
-local error_schema    = {
+local error_schema       = {
   description = "Error page to return when APISIX returns a specific status codes.",
   type = "object",
   additionalProperties = false,
@@ -64,7 +63,7 @@ local error_schema    = {
   },
 }
 
-local metadata_schema = {
+local metadata_schema    = {
   type = "object",
   additionalProperties = false,
   patternProperties = {
@@ -85,13 +84,13 @@ local metadata_schema = {
   },
 }
 
-local schema          = {
+local schema             = {
   type = "object",
   properties = {},
 }
 
-local _M              = {
-  version = 0.1,
+local _M                 = {
+  version = 1.0,
   priority = 0,
   name = plugin_name,
   schema = schema,
@@ -100,77 +99,81 @@ local _M              = {
   author = plugin_author,
 }
 
--- Reference: https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
-local status_text     = {
-  ["100"] = "Continue",
-  ["101"] = "Switching Protocols",
-  ["102"] = "Processing",
-  ["103"] = "Early Hints",
+-- Reference: https://apisix.apache.org/docs/apisix/plugin-develop/#register-custom-variable
+core.ctx.register_var("status_text", function(ctx)
+  -- Reference: https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+  local status_texts = {
+    ["100"] = "Continue",
+    ["101"] = "Switching Protocols",
+    ["102"] = "Processing",
+    ["103"] = "Early Hints",
 
-  ["200"] = "OK",
-  ["201"] = "Created",
-  ["202"] = "Accepted",
-  ["203"] = "Non-Authoritative Information",
-  ["204"] = "No Content",
-  ["205"] = "Reset Content",
-  ["206"] = "Partial Content",
-  ["207"] = "Multi-Status",
-  ["208"] = "Already Reported",
-  ["226"] = "IM Used",
+    ["200"] = "OK",
+    ["201"] = "Created",
+    ["202"] = "Accepted",
+    ["203"] = "Non-Authoritative Information",
+    ["204"] = "No Content",
+    ["205"] = "Reset Content",
+    ["206"] = "Partial Content",
+    ["207"] = "Multi-Status",
+    ["208"] = "Already Reported",
+    ["226"] = "IM Used",
 
-  ["300"] = "Multiple Choices",
-  ["301"] = "Moved Permanently",
-  ["302"] = "Found",
-  ["303"] = "See Other",
-  ["304"] = "Not Modified",
-  ["305"] = "Use Proxy",
-  ["307"] = "Temporary Redirect",
-  ["308"] = "Permanent Redirect",
+    ["300"] = "Multiple Choices",
+    ["301"] = "Moved Permanently",
+    ["302"] = "Found",
+    ["303"] = "See Other",
+    ["304"] = "Not Modified",
+    ["305"] = "Use Proxy",
+    ["307"] = "Temporary Redirect",
+    ["308"] = "Permanent Redirect",
 
-  ["400"] = "Bad Request",
-  ["401"] = "Unauthorized",
-  ["402"] = "Payment Required",
-  ["403"] = "Forbidden",
-  ["404"] = "Not Found",
-  ["405"] = "Method Not Allowed",
-  ["406"] = "Not Acceptable",
-  ["407"] = "Proxy Authentication Required",
-  ["408"] = "Request Timeout",
-  ["409"] = "Conflict",
-  ["410"] = "Gone",
-  ["411"] = "Length Required",
-  ["412"] = "Precondition Failed",
-  ["413"] = "Content Too Large",
-  ["414"] = "URI Too Long",
-  ["415"] = "Unsupported Media Type",
-  ["416"] = "Range Not Satisfiable",
-  ["417"] = "Expectation Failed",
-  ["418"] = "I'm a teapot",
-  ["421"] = "Misdirected Request",
-  ["422"] = "Unprocessable Content",
-  ["423"] = "Locked",
-  ["424"] = "Failed Dependency",
-  ["425"] = "Too Early",
-  ["426"] = "Upgrade Required",
-  ["428"] = "Precondition Required",
-  ["429"] = "Too Many Requests",
-  ["431"] = "Request Header Fields Too Large",
-  ["451"] = "Unavailable For Legal Reasons",
+    ["400"] = "Bad Request",
+    ["401"] = "Unauthorized",
+    ["402"] = "Payment Required",
+    ["403"] = "Forbidden",
+    ["404"] = "Not Found",
+    ["405"] = "Method Not Allowed",
+    ["406"] = "Not Acceptable",
+    ["407"] = "Proxy Authentication Required",
+    ["408"] = "Request Timeout",
+    ["409"] = "Conflict",
+    ["410"] = "Gone",
+    ["411"] = "Length Required",
+    ["412"] = "Precondition Failed",
+    ["413"] = "Content Too Large",
+    ["414"] = "URI Too Long",
+    ["415"] = "Unsupported Media Type",
+    ["416"] = "Range Not Satisfiable",
+    ["417"] = "Expectation Failed",
+    ["418"] = "I'm a teapot",
+    ["421"] = "Misdirected Request",
+    ["422"] = "Unprocessable Content",
+    ["423"] = "Locked",
+    ["424"] = "Failed Dependency",
+    ["425"] = "Too Early",
+    ["426"] = "Upgrade Required",
+    ["428"] = "Precondition Required",
+    ["429"] = "Too Many Requests",
+    ["431"] = "Request Header Fields Too Large",
+    ["451"] = "Unavailable For Legal Reasons",
 
-  ["500"] = "Internal Server Error",
-  ["501"] = "Not Implemented",
-  ["502"] = "Bad Gateway",
-  ["503"] = "Service Unavailable",
-  ["504"] = "Gateway Timeout",
-  ["505"] = "HTTP Version Not Supported",
-  ["506"] = "Variant Also Negotiates",
-  ["507"] = "Insufficient Storage",
-  ["508"] = "Loop Detected",
-  ["510"] = "Not Extended",
-  ["511"] = "Network Authentication Required",
+    ["500"] = "Internal Server Error",
+    ["501"] = "Not Implemented",
+    ["502"] = "Bad Gateway",
+    ["503"] = "Service Unavailable",
+    ["504"] = "Gateway Timeout",
+    ["505"] = "HTTP Version Not Supported",
+    ["506"] = "Variant Also Negotiates",
+    ["507"] = "Insufficient Storage",
+    ["508"] = "Loop Detected",
+    ["510"] = "Not Extended",
+    ["511"] = "Network Authentication Required",
 
-  default = "Something is wrong"
-}
+    default = "Something is wrong"
+  }
+  return status_texts[ngx.var.status] or status_texts.default
+end)
 
 local function make_response(error)
   local response = {}
@@ -234,7 +237,6 @@ function _M.header_filter(_, ctx)
     end
 
     -- Parse NGiNX variables
-    ctx.var.status_text = status_text[ngx.var.status] or status_text.default
     custom_response.body = apisix_utils.resolve_var(custom_response.body, ctx.var)
 
     -- Set Content-Length header before body_phase
